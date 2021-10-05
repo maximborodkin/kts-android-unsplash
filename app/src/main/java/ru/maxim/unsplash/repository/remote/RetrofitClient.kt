9d42e49ef75261b5658e9ac86fae04f6728b452c
maximborodkin <maxim.borodkin1999@yandex.ru>
2021-10-05 19:42:35 +0300

    add auth interceptor

--- app/src/main/java/ru/maxim/unsplash/repository/remote/RetrofitClient.kt
+++ app/src/main/java/ru/maxim/unsplash/repository/remote/RetrofitClient.kt
@@ -6,26 +6,32 @@ import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
 import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.maxim.unsplash.repository.remote.service.CollectionService
 import ru.maxim.unsplash.repository.remote.service.PhotoService
 
 object RetrofitClient {
     private const val baseUrl = "https://api.unsplash.com/"
 
     private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().apply { level = BASIC })
+        .addInterceptor(HttpLoggingInterceptor().apply { level = BODY })
         .addInterceptor { chain: Interceptor.Chain ->
-            val request = chain.request().newBuilder()
-                .addHeader("Authorization", "Bearer ${AuthConfig.accessToken}")
-                .build()
-            chain.proceed(request)
+            if (AuthConfig.accessToken.isNullOrBlank()) {
+                chain.proceed(chain.request())
+            } else {
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer ${AuthConfig.accessToken}")
+                    .build()
+                chain.proceed(request)
+            }
         }
         .build()
 
-    val instance = Retrofit.Builder()
+    private val instance: Retrofit = Retrofit.Builder()
         .client(okHttpClient)
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val photoService by lazy { instance.create(PhotoService::class.java) }
+    val photoService: PhotoService by lazy { instance.create(PhotoService::class.java) }
+    val collectionService: CollectionService by lazy { instance.create(CollectionService::class.java) }
 }

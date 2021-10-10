fd8f9612de2c09147c773d82f04cbb184ea38235
maximborodkin <maxim.borodkin1999@yandex.ru>
2021-10-10 22:06:32 +0300

    fix CacheGlideUrl

--- app/src/main/java/ru/maxim/unsplash/util/CacheGlideUrl.kt
+++ app/src/main/java/ru/maxim/unsplash/util/CacheGlideUrl.kt
@@ -1,11 +1,12 @@
 package ru.maxim.unsplash.util
 
 import com.bumptech.glide.load.model.GlideUrl
+import java.net.URL
 
 class CacheGlideUrl(private val url: String) : GlideUrl(url) {
 
     override fun getCacheKey(): String =
-        if(url.contains("?")) {
-            url.split("?").first()
+        if(url.contains("ixid=")) {
+            url.replace("ixid=\\w+&", "")
         } else url
 }
